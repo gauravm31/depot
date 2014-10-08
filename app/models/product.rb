40b015a2034567e7d5626f5652d08598e1309441
Gaurav Mahajan <gaurav@vinsol.com>
2014-10-08 19:18:53 +0530

    Add basic validations.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(png|jpg|gif)\Z}i,
+    message: 'Must be a URL for PNG, JPG, GIF image.'
+  }
 end
